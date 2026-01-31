8f5f1d63d4d7af09c88333400051b0a2d1a91578
Egor <ifbytopic@gmail.com>
2026-01-31 23:12:53 +0700

    refactor(board): добавление поддержки разных стилей отрисовки

--- board.py
+++ board.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-
 import numpy as np
 from typing import Tuple, Optional, List
-
 from constants import BOARD_SIZE, EMPTY
 
 
@@ -17,7 +15,6 @@ class Board:
             return False
         if self.grid[row, col] != EMPTY:
             return False
-
         self.grid[row, col] = player
         self.last_move = (row, col, player)
         return True
@@ -34,17 +31,13 @@ class Board:
         if self.last_move is None:
             return False
         r, c, last_player = self.last_move
-
         if last_player != player:
             return False
-
         directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
         self.win_line = [(r, c)]
-
         for dr, dc in directions:
             count = 1
             line = [(r, c)]
-
             for sign in (1, -1):
                 nr, nc = r + dr * sign, c + dc * sign
                 while (0 <= nr < BOARD_SIZE and 0 <= nc < BOARD_SIZE and
@@ -53,18 +46,11 @@ class Board:
                     line.append((nr, nc))
                     nr += dr * sign
                     nc += dc * sign
-
             if count >= 5:
                 self.win_line = line
                 return True
-
         self.win_line = None
         return False
 
     def get_empty_cells(self) -> List[Tuple[int, int]]:
-        return [
-            (r, c)
-            for r in range(BOARD_SIZE)
-            for c in range(BOARD_SIZE)
-            if self.grid[r, c] == EMPTY
-        ]
+        return [(r, c) for r in range(BOARD_SIZE) for c in range(BOARD_SIZE) if self.grid[r, c] == EMPTY]
